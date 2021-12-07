baaccdbbf5e62d9bdff10a465aca31deafe1f9ab
HoangHieu <hakunhan@gmail.com>
2021-12-07 17:18:21 +0700

    convert package manager to yarn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'time-management-ea4a9',
+    appId: '1:568448802220:web:2f82f4aef3cc910fa86e09',
+    storageBucket: 'time-management-ea4a9.appspot.com',
+    apiKey: 'AIzaSyCT_dNzHkdQcYdMPDmu2hu8sIjs-2wJUzs',
+    authDomain: 'time-management-ea4a9.firebaseapp.com',
+    messagingSenderId: '568448802220',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'time-management-ea4a9',
+    appId: '1:568448802220:web:2f82f4aef3cc910fa86e09',
+    storageBucket: 'time-management-ea4a9.appspot.com',
+    apiKey: 'AIzaSyCT_dNzHkdQcYdMPDmu2hu8sIjs-2wJUzs',
+    authDomain: 'time-management-ea4a9.firebaseapp.com',
+    messagingSenderId: '568448802220',
+  },
   production: false
 };
 
